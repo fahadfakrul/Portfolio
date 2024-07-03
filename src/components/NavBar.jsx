1705afba7fe587175a769617dd2d9ca74b1061f7
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-07-03 16:44:29 +0600

    add: navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import logo from '../assets/logo.png'
+import { FaSquareXTwitter } from 'react-icons/fa6';
+
+const NavBar = () => {
+    return (
+        
+            <nav className="mb-20 flex items-center justify-between py-6">
+                <div className="flex flex-shrink-0 items-center">
+                 <img src={logo} alt="" />
+                </div>
+                <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+                    <FaGithub></FaGithub>
+                    <FaLinkedin></FaLinkedin>
+                    <FaSquareXTwitter></FaSquareXTwitter>
+                </div>
+            </nav>
+       
+        
+    );
+};
+
+export default NavBar;
