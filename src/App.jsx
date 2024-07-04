58810dff033bf0ea4558808a3d6492a78eae91f5
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-07-04 17:12:52 +0600

    add: navbar navigation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
@@ -8,18 +9,28 @@ import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
 const App = () => {
+  const aboutRef = useRef(null);
+  const techRef = useRef(null);
+  const eduRef = useRef(null);
+  const projRef = useRef(null);
+  const contactRef = useRef(null);
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div></div>
       <div className="container mx-auto px-8">
-        <NavBar></NavBar>
+        <NavBar
+        aboutRef={aboutRef}
+        techRef={techRef}
+        eduRef={eduRef}
+        projRef={projRef}
+        contactRef={contactRef}></NavBar>
         <Hero></Hero>
-        <About></About>
-        <Technologies></Technologies>
-        <Education></Education>
-        <Projects></Projects>
-        <Contact></Contact>
+        <div id="about" ref={aboutRef}><About /></div>
+        <div id="technologies" ref={techRef}><Technologies /></div>
+        <div id="education" ref={eduRef}><Education /></div>
+        <div id="projects" ref={projRef}><Projects /></div>
+        <div id="contact" ref={contactRef}><Contact /></div>
         <Footer></Footer>
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,23 @@
+
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 
-const NavBar = () => {
+const NavBar = ({ aboutRef, techRef, eduRef, projRef, contactRef }) => {
+
+    const scrollToSection = (ref) => {
+        ref.current.scrollIntoView({ behavior: 'smooth' });
+      };
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <h1 className="font-extrabold text-6xl text-white  font-mono">F</h1>
+      </div>
+      <div className="hidden md:flex gap-3">
+        <a className="border-b" href="#about" onClick={() => scrollToSection(aboutRef)}>About Me</a>
+        <a className="border-b" href="#technologies" onClick={() => scrollToSection(techRef)}>Technologies</a>
+        <a className="border-b" href="#education" onClick={() => scrollToSection(eduRef)}>Education</a>
+        <a className="border-b" href="#projects" onClick={() => scrollToSection(projRef)}>Projects</a>
+        <a className="border-b" href="#contact" onClick={() => scrollToSection(contactRef)}>Contact</a>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
        <a className="transition hover:scale-110" href="https://github.com/fahadfakrul" target="blank"> <FaGithub ></FaGithub></a>
